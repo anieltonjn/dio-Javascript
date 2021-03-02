e5e4fe304576032023930b9a0a6bb92892f62369
Anielton José <anieltonjn@gmail.com>
2021-03-02 13:50:09 -0300

    Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
-var nome = "Anielton José";
-var n1 = 5;
-var n2 = 3;
-var frase = "Japão é o melhor time do mundo";
 
+
+var fruta = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista.length);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+//var nome = "Anielton José";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
 //alert(nome + " tem " + idade);
 //alert(idade + idade2);
-console.log(nome);
-console.log(n1 * n2);
-console.log(frase.replace("Japão","Brasil"));
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.replace("Japão","Brasil"));
 //alert(frade.replace("Japão","Brasil"));
