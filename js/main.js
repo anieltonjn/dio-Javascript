9048cc4e08a95968c952a84d3d84b859b7d928f6
Anielton José <anieltonjn@gmail.com>
2021-03-02 13:39:23 -0300

    Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Anielton José";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+
+//alert(nome + " tem " + idade);
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.replace("Japão","Brasil"));
+//alert(frade.replace("Japão","Brasil"));
